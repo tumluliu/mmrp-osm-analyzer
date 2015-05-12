8febf6ffa9d164c78f09135e26716b8b78e5349e
tumluliu <nudtlliu@gmail.com>
2015-05-12 22:58:51 +0800

    combine the standalone CSV files into a single one

---
+++ src/combine_gpx_csv_info.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CSV_DIR=$1
+DEST_CSV_FILE=$2
+FILE_INDEX=0
+
+FILE_COUNT=$(ls -l $CSV_DIR/*.csv | grep -v ^l | wc -l)
+echo "Concatenating CSV files... "
+for csv_f in "$CSV_DIR"/*.csv
+do
+    echo -ne "Processing $FILE_INDEX / $FILE_COUNT\r\c"
+    if [ FILE_INDEX == 0 ]
+    then
+        cat csv_f > $DEST_CSV_FILE
+    else
+        tail -n +2 csv_f >> $DEST_CSV_FILE
+    fi
+    let "FILE_INDEX+=1"
+done
+echo "done!"
+
