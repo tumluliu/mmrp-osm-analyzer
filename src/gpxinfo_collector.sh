10997b7113045ec60f74717073d6ea31879b8bfb
tumluliu <nudtlliu@gmail.com>
2015-05-12 23:04:14 +0800

    use cmd line parameter to pass data dir instead of using hard code in the script

--- src/gpxinfo_collector.sh
+++ src/gpxinfo_collector.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Scan and collect the summaries of all the downloaded GPX files via gpxinfo util
 
-gpx_dir=/Users/user/Research/data/GPX/Munich
+#gpx_dir=/Users/user/Research/data/GPX/Munich
+gpx_dir=$1
 
 for gpx_file in "$gpx_dir"/gpx_with_elevations/*.gpx
 do
     echo "collecting summary info of $gpx_file ..."
     gpxinfo "$gpx_file" > "$gpx_file.summary"
-    echo "done! "
+    echo " done!"
 done
 mkdir -p "$gpx_dir"/summary
 mv "$gpx_dir"/gpx_with_elevations/*.summary "$gpx_dir"/summary
