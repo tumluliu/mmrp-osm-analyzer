e15265bf11b9e32b49d05772c286f804f6a2482c
tumluliu <nudtlliu@gmail.com>
2015-05-14 13:50:51 +0800

    fix bug on variable FILE_INDEX and csv_f without writing $ symbol

--- src/combine_gpx_csv_info.sh
+++ src/combine_gpx_csv_info.sh
@@ -2,18 +2,18 @@
 
 CSV_DIR=$1
 DEST_CSV_FILE=$2
-FILE_INDEX=0
+FILE_INDEX=1
 
 FILE_COUNT=$(ls -l $CSV_DIR/*.csv | grep -v ^l | wc -l)
 echo "Concatenating CSV files... "
 for csv_f in "$CSV_DIR"/*.csv
 do
-    echo -ne "Processing $FILE_INDEX / $FILE_COUNT\r\c"
-    if [ FILE_INDEX == 0 ]
+    echo -ne "$FILE_INDEX / $FILE_COUNT\r\c"
+    if [ $FILE_INDEX == 1 ]
     then
-        cat csv_f > $DEST_CSV_FILE
+        cat "$csv_f" > "$DEST_CSV_FILE"
     else
-        tail -n +2 csv_f >> $DEST_CSV_FILE
+        tail -n +2 "$csv_f" >> "$DEST_CSV_FILE"
     fi
     let "FILE_INDEX+=1"
 done
