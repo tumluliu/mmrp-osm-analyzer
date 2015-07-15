bfdae7c1377904e3de822ec0eea36becd124c10e
tumluliu <nudtlliu@gmail.com>
2015-07-16 01:05:06 +0800

    refactor var name

--- src/gpx_summary_reducer.py
+++ src/gpx_summary_reducer.py
@@ -23,8 +23,8 @@ with open(CONFIG_FILE) as colname_file:
 
 gpx_sum_files = [f for f in listdir(GPX_SUMMARY_DIR) if isfile(join(GPX_SUMMARY_DIR, f))]
 # There are normally 12 lines of summary info for each Track Segment
-track_info_lines = 16
-track_info = OrderedDict()
+track_seg_info_lines = 16
+track_seg_info = OrderedDict()
 na_string_list = ["n/a", "None", "none", "N/A", "NA"]
 
 for gpx_sum_file in gpx_sum_files:
@@ -50,78 +50,78 @@ for gpx_sum_file in gpx_sum_files:
             # info block
             is_track_recorder_on = True
             for s in line.strip().split(','):
-                track_info[colnames[s.split()[0]]] = s.split()[1][1:]
+                track_seg_info[colnames[s.split()[0]]] = s.split()[1][1:]
             track_cursor += 2
-        elif (is_track_recorder_on) and (track_cursor <= track_info_lines):
+        elif (is_track_recorder_on) and (track_cursor <= track_seg_info_lines):
             key = colnames[line.strip().split(':')[0]]
             if re.match(r".*Length 2D.*", line):
-                track_info[key] = \
+                track_seg_info[key] = \
                     str(float(line.strip().split(':')[1][1:-2]) * 1000.)
                 track_cursor += 1
             if re.match(r".*Length 3D.*", line):
-                track_info[key] = \
+                track_seg_info[key] = \
                     str(float(line.strip().split(':')[1][1:-2]) * 1000.)
                 track_cursor += 1
             if re.match(r".*Moving time.*", line):
                 if line.strip().split(':')[1][1:] in na_string_list:
-                    track_info[key] = "NA"
+                    track_seg_info[key] = "NA"
                 else:
-                    track_info[key] = line.strip().split(':', 1)[1][1:]
+                    track_seg_info[key] = line.strip().split(':', 1)[1][1:]
                 track_cursor += 1
             if re.match(r".*Stopped time.*", line):
                 if line.strip().split(':')[1][1:] in na_string_list:
-                    track_info[key] = "NA"
+                    track_seg_info[key] = "NA"
                 else:
-                    track_info[key] = line.strip().split(':', 1)[1][1:]
+                    track_seg_info[key] = line.strip().split(':', 1)[1][1:]
                 track_cursor += 1
             if re.match(r".*Max speed.*", line):
-                track_info[key] = line.strip().split(':')[1].split('=')[0][1:-4]
+                track_seg_info[key] = line.strip().split(':')[1].split('=')[0][1:-4]
                 track_cursor += 1
             if re.match(r".*Total uphill.*", line):
-                track_info[key] = line.strip().split(':')[1][1:-1]
+                track_seg_info[key] = line.strip().split(':')[1][1:-1]
                 track_cursor += 1
             if re.match(r".*Total downhill.*", line):
-                track_info[key] = line.strip().split(':')[1][1:-1]
+                track_seg_info[key] = line.strip().split(':')[1][1:-1]
                 track_cursor += 1
             if re.match(r".*Started.*", line):
                 if line.strip().split(':', 1)[1][1:] in na_string_list:
-                    track_info[key] = "NA"
+                    track_seg_info[key] = "NA"
                 else:
-                    track_info[key] = line.strip().split(':', 1)[1][1:]
+                    track_seg_info[key] = line.strip().split(':', 1)[1][1:]
                 track_cursor += 1
             if re.match(r".*Ended.*", line):
                 if line.strip().split(':', 1)[1][1:] in na_string_list:
-                    track_info[key] = "NA"
+                    track_seg_info[key] = "NA"
                 else:
-                    track_info[key] = line.strip().split(':', 1)[1][1:]
+                    track_seg_info[key] = line.strip().split(':', 1)[1][1:]
                 track_cursor += 1
             if re.match(r".*Points.*", line):
-                track_info[key] = line.strip().split(':')[1][1:]
+                track_seg_info[key] = line.strip().split(':')[1][1:]
                 track_cursor += 1
             if re.match(r".*Start location longitude.*", line):
-                track_info[key] = line.strip().split(':')[1][1:]
+                track_seg_info[key] = line.strip().split(':')[1][1:]
                 track_cursor += 1
             if re.match(r".*Start location latitude.*", line):
-                track_info[key] = line.strip().split(':')[1][1:]
+                track_seg_info[key] = line.strip().split(':')[1][1:]
                 track_cursor += 1
             if re.match(r".*End location longitude.*", line):
-                track_info[key] = line.strip().split(':')[1][1:]
+                track_seg_info[key] = line.strip().split(':')[1][1:]
                 track_cursor += 1
             if re.match(r".*End location latitude.*", line):
-                track_info[key] = line.strip().split(':')[1][1:]
+                track_seg_info[key] = line.strip().split(':')[1][1:]
                 track_cursor += 1
             if re.match(r".*Avg distance between points.*", line):
-                track_info[key] = line.strip().split(':')[1][1:-1]
+                track_seg_info[key] = line.strip().split(':')[1][1:-1]
                 track_cursor += 1
-        if (is_track_recorder_on) and (track_cursor == track_info_lines + 1):
+        if (is_track_recorder_on) and (track_cursor == track_seg_info_lines + 1):
             #print " A track segment has been processed!"
-            #print str(",".join(track_info.values()))
+            #print str(",".join(track_seg_info.values()))
             csv_f.write(str(page_no) + "," + \
-                        str(",".join(track_info.values())) + "\n")
-            #print track_info.values()
+                        str(",".join(track_seg_info.values())) + "\n")
+            #print track_seg_info.values()
             is_track_recorder_on = False
             track_cursor = 0
-            track_info.clear()
+            track_seg_info.clear()
     csv_f.close()
     f.close()
     print " done!"
