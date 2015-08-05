f0bbd55973f18bfdd83fa6cbddc7c116c5e8c765
tumluliu <nudtlliu@gmail.com>
2015-08-06 00:38:49 +0800

    reproject the coordinates of tracks and multimodal paths from EPSG:4326 to EPSG:25832 to make the distance calculation reasonable. Many thanks for the help from SimilarityMeasures package's author Kevin Toohey

--- src/similarity_calculator.R
+++ src/similarity_calculator.R
@@ -3,40 +3,133 @@ library(rgdal)
 library(data.table)
 
 MakeTrackTuple <- function(f) {
-    fid <- unlist(strsplit(tail(unlist(strsplit(f, "/")), n=1), "_"))[1]
-    geom <- readOGR(f, layer="OGRGeoJSON", require_geomType="wkbLineString")
+    fid <- unlist(strsplit(tail(unlist(
+        strsplit(f, "/")
+    ), n = 1), "_"))[1]
+    geom <-
+        readOGR(f, layer = "OGRGeoJSON", require_geomType = "wkbLineString")
     return(c(fid, geom))
 }
 
 MakePathTuple <- function(f) {
-    fileid <- unlist(strsplit(tail(unlist(strsplit(f, "/")), n=1), "[.]"))[1]
-    fid <- unlist(strsplit(tail(unlist(strsplit(f, "/")), n=1), "_"))[1]
-    geom <- tryCatch(readOGR(f, layer="OGRGeoJSON", require_geomType="wkbLineString"), 
-                     error=function(e) NA) 
-    if (!is.na(geom)) return(c(fileid, fid, geom)) else return(NA)
+    fileid <-
+        unlist(strsplit(tail(unlist(
+            strsplit(f, "/")
+        ), n = 1), "[.]"))[1]
+    fid <-
+        unlist(strsplit(tail(unlist(
+            strsplit(f, "/")
+        ), n = 1), "_"))[1]
+    geom <-
+        tryCatch(
+            readOGR(f, layer = "OGRGeoJSON", require_geomType = "wkbLineString"),
+            error = function(e)
+                NA
+        )
+    if (!is.na(geom))
+        return(c(fileid, fid, geom))
+    else
+        return(NA)
 }
 
-CalculateSimilarity <- function(track.coords, path.coords, FUN=SimilarityMeasures::Frechet, ...) {
-    track.path <- lapply(path.coords, function(p) list(fileid=p[["fileid"]],
-                                                       fid=p[["fid"]], 
-                                                       path=p[["coords"]], 
-                                                       track=track.coords[sapply(track.coords, function(t) t[["fid"]]==p[["fid"]])] 
-                                                       ))
-    track.path <- track.path[sapply(track.path, function(tp) length(tp[["track"]]) > 0)]
-    return(lapply(track.path, function(tp) list(fileid=tp[["fileid"]], 
-                                                fid=tp[["fid"]], 
-                                                dist=FUN(tp[["path"]], tp[["track"]][[1]][["coords"]], ...))))
-}
+CalculateDTW <-
+    function(fileid, fid, path, track) {
+        d <- SimilarityMeasures::DTW(path, track)
+        return(list(
+            fileid = fileid, fid = fid, dist = d,
+            ratio = d / (max(nrow(path), nrow(track)) - 2)
+        ))
+    }
 
-track.filenames <- list.files("data/sample_tracks", pattern="*.geojson", full.names=TRUE)
+CalculateEditDist <-
+    function(fileid, fid, path, track, ...) {
+        d <- SimilarityMeasures::EditDist(path, track, ...)
+        return(list(
+            fileid = fileid, fid = fid, dist = d,
+            ratio = d / (max(nrow(path), nrow(track)) - 2)
+        ))
+    }
+
+# EPSG:25832
+munich.srs <-
+    "+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+track.filenames <-
+    list.files("data/sample_tracks", pattern = "*.geojson", full.names = TRUE)
 track.geom <- lapply(track.filenames, MakeTrackTuple)
-track.coords <- lapply(track.geom, function(t) list(fid=t[[1]], coords=t[[2]]@lines[[1]]@Lines[[1]]@coords))
-path.filenames <- list.files("data/sample_paths", pattern = "*.geojson", full.names = TRUE)
+tl0 <-
+    lapply(track.geom, function(x)
+        list(x[[1]], x[[2]]@lines[[1]]@Lines))
+tl1 <-
+    lapply(tl0, function(y)
+        list(y[[1]], lapply(y[[2]], function(z)
+            z@coords)))
+track.coords <-
+    lapply(d1, function(t)
+        list(fid = t[[1]], coords = do.call(rbind, t[[2]])))
+track.coords.reproj <-
+    lapply(track.coords, function(t)
+        list(fid = t[["fid"]],
+             coords = project(t[["coords"]],
+                              munich.srs)))
+path.filenames <-
+    list.files("data/sample_paths", pattern = "*.geojson", full.names = TRUE)
 path.geom <- lapply(path.filenames, MakePathTuple)
 path.geom <- path.geom[(!is.na(path.geom))]
-ll0 <- lapply(path.geom, function(x) list(x[[1]], x[[2]], x[[3]]@lines))
-ll1 <- lapply(ll0, function(y) list(y[[1]], y[[2]], lapply(y[[3]], function(z) z@Lines[[1]]@coords)))
-path.coords <- lapply(ll1, function(p) list(fileid=p[[1]], fid=p[[2]], coords=do.call(rbind, p[[3]])))
-dtw <- CalculateSimilarity(track.coords, path.coords, FUN=SimilarityMeasures::DTW)
-editdist <- CalculateSimilarity(track.coords, path.coords, FUN=SimilarityMeasures::EditDist, pointDistance=0.001)
+ll0 <-
+    lapply(path.geom, function(x)
+        list(x[[1]], x[[2]], x[[3]]@lines))
+ll1 <-
+    lapply(ll0, function(y)
+        list(y[[1]], y[[2]], lapply(y[[3]], function(z)
+            z@Lines[[1]]@coords)))
+path.coords <-
+    lapply(ll1, function(p)
+        list(
+            fileid = p[[1]], fid = p[[2]], coords = do.call(rbind, p[[3]])
+        ))
+path.coords.reproj <-
+    lapply(path.coords, function(p)
+        list(
+            fileid = p[["fileid"]],
+            fid = p[["fid"]],
+            coords = project(p[["coords"]],
+                             munich.srs)
+        ))
+track.path <- lapply(path.coords.reproj,
+                     function(p)
+                         list(
+                             fileid = p[["fileid"]],
+                             fid = p[["fid"]],
+                             path = p[["coords"]],
+                             track = track.coords.reproj[sapply(track.coords.reproj,
+                                                                function(t)
+                                                                    t[["fid"]] == p[["fid"]])]
+                         ))
+track.path <-
+    track.path[sapply(track.path, function(tp)
+        length(tp[["track"]]) > 0)]
+print("Calculating DTW distance...")
+dtw <- lapply(track.path, function(tp)
+    CalculateDTW(tp[["fileid"]],
+                 tp[["fid"]],
+                 tp[["path"]],
+                 tp[["track"]][[1]][["coords"]]))
+print("done!")
+dtw.df <- do.call(rbind, dtw)
+print("Writing DTW data to CSV file...")
+write.csv(dtw.df, file = "data/dtw.csv", row.names = FALSE)
+print("done!")
+print("Calculating EditDist distance...")
+editdist <- lapply(track.path,
+                   function(tp)
+                       CalculateEditDist(tp[["fileid"]],
+                                         tp[["fid"]],
+                                         tp[["path"]],
+                                         tp[["track"]][[1]][["coords"]],
+                                         pointDistance = 50))
+print("done!")
+editdist.df <- do.call(rbind, editdist)
+print("Writing EditDist data to CSV file...")
+write.csv(editdist.df, file = "data/editdist.csv", row.names = FALSE)
+print("done!")
 #frechet <- CalculateSimilarity(track.coords, path.coords, FUN=SimilarityMeasures::Frechet)
